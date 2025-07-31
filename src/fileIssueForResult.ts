513d7ac60d7d2a80b6c82f1fff17c186fb110ed6
Clay Miller <clay@smockle.com>
2025-07-31 13:38:07 -0400

    fix: Format 'result.solutionLong' as a list

--- src/fileIssueForResult.ts
+++ src/fileIssueForResult.ts
@@ -7,11 +7,12 @@ export async function fileIssueForResult(octokit: Octokit, repoWithOwner: string
   const owner = repoWithOwner.split('/')[0];
   const repo = repoWithOwner.split('/')[1];
   const title = `Accessibility issue: ${result.problemShort[0].toUpperCase() + result.problemShort.slice(1)} on ${new URL(result.url).pathname}`;
+  const solutionLong = result.solutionLong?.split('\n').map((line) => { if (!line.trim().startsWith("Fix any") && !line.trim().startsWith("Fix all")) { return `- ${line}`; } else { return line; } }).join('\n');
   const body = `
 An accessibility scan flagged the element \`${result.html}\` on ${result.url} because ${result.problemShort}. Learn more about why this was flagged by visiting ${result.problemUrl}.
 
 To fix this, ${result.solutionShort}.
-${result.solutionLong ? `\nSpecifically:\n\n${result.solutionLong}` : ''}
+${solutionLong ? `\nSpecifically:\n\n${solutionLong}` : ''}
 `;
 
   return await octokit.request(`POST /repos/${owner}/${repo}/issues`, {
