8e081bfaac91463812f16d4c441f246c4c35c649
Clay Miller <clay@smockle.com>
2025-10-10 15:37:17 -0400

    fix: Clean-up Issue class: Reposition 'constructor', rename 'parseIssue', cache its result, and update the cache if 'url' changes

--- .github/actions/file/src/Issue.ts
+++ .github/actions/file/src/Issue.ts
@@ -1,44 +1,16 @@
 import type { Issue as IssueInput } from "./types.d.js";
 
 export class Issue implements IssueInput {
-  /**
-   * Extracts owner, repository, and issue number from a GitHub issue URL.
-   * @param issueUrl A GitHub issue URL (e.g. `https://github.com/owner/repo/issues/42`).
-   * @returns An object with `owner`, `repository`, and `issueNumber` keys.
-   * @throws The provided URL is unparseable due to its unexpected format.
-   */
-  static parseIssueUrl(issueUrl: string): {
+  #url!: string;
+  #parsedUrl!: {
     owner: string;
     repository: string;
     issueNumber: number;
-  } {
-    const { owner, repository, issueNumber } =
-      /\/(?<owner>[^/]+)\/(?<repository>[^/]+)\/issues\/(?<issueNumber>\d+)(?:[/?#]|$)/.exec(
-        issueUrl
-      )?.groups || {};
-    if (!owner || !repository || !issueNumber) {
-      throw new Error(`Could not parse issue URL: ${issueUrl}`);
-    }
-    return { owner, repository, issueNumber: Number(issueNumber) };
-  }
-
-  url: string;
+  };
   nodeId: string;
   id: number;
   title: string;
   state?: "open" | "reopened" | "closed";
-
-  get owner(): string {
-    return Issue.parseIssueUrl(this.url).owner;
-  }
-
-  get repository(): string {
-    return Issue.parseIssueUrl(this.url).repository;
-  }
-
-  get issueNumber(): number {
-    return Issue.parseIssueUrl(this.url).issueNumber;
-  }
 
   constructor({ url, nodeId, id, title, state }: IssueInput) {
     this.url = url;
@@ -46,5 +18,46 @@ export class Issue implements IssueInput {
     this.id = id;
     this.title = title;
     this.state = state;
+  }
+
+  set url(newUrl: string) {
+    this.#url = newUrl;
+    this.#parsedUrl = this.#parseUrl();
+  }
+
+  get url(): string {
+    return this.#url;
+  }
+
+  get owner(): string {
+    return this.#parsedUrl.owner;
+  }
+
+  get repository(): string {
+    return this.#parsedUrl.repository;
+  }
+
+  get issueNumber(): number {
+    return this.#parsedUrl.issueNumber;
+  }
+
+  /**
+   * Extracts owner, repository, and issue number from the Issue instance’s GitHub issue URL.
+   * @returns An object with `owner`, `repository`, and `issueNumber` keys.
+   * @throws The provided URL is unparseable due to its unexpected format.
+   */
+  #parseUrl(): {
+    owner: string;
+    repository: string;
+    issueNumber: number;
+  } {
+    const { owner, repository, issueNumber } =
+      /\/(?<owner>[^/]+)\/(?<repository>[^/]+)\/issues\/(?<issueNumber>\d+)(?:[/?#]|$)/.exec(
+        this.#url
+      )?.groups || {};
+    if (!owner || !repository || !issueNumber) {
+      throw new Error(`Could not parse issue URL: ${this.#url}`);
+    }
+    return { owner, repository, issueNumber: Number(issueNumber) };
   }
 }
