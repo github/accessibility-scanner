09df8b1bab4485fef075c6044be0e330b1c0b710
Abdul Ahmad <abdulahmad307@github.com>
2025-12-18 12:33:36 -0500

    Change page load wait condition to 'load'

--- .github/actions/find/src/findForUrl.ts
+++ .github/actions/find/src/findForUrl.ts
@@ -9,7 +9,7 @@ export async function findForUrl(url: string, authContext?: AuthContext): Promis
   const context = await browser.newContext(contextOptions);
   const page = await context.newPage();
   await page.goto(url, { 
-    waitUntil: 'domcontentloaded',
+    waitUntil: 'load',
     // - looks like default timeout is 3000ms
     // - increasing for testing
     timeout: 60000,
