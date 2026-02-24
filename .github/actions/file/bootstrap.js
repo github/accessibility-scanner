71774c87bda9fb5a72aad79a52c280dc624ca821
Joyce Zhu <joycezhu@github.com>
2026-02-24 21:14:24 +0000

    Add more error logging to Actions
    Looks like we need to explicitly call
    `core.setFailed` before `process.exit(1)` (aka
    what we use if anything goes wrong) if we want
    this to be readable in Actions logs.

--- .github/actions/file/bootstrap.js
+++ .github/actions/file/bootstrap.js
@@ -3,22 +3,24 @@
 
 import fs from 'node:fs'
 import * as url from 'node:url'
-import { spawn } from 'node:child_process'
+import {spawn} from 'node:child_process'
 
-function spawnPromisified(command, args, { quiet = false, ...options } = {}) {
+import * as core from '@actions/core'
+
+function spawnPromisified(command, args, {quiet = false, ...options} = {}) {
   return new Promise((resolve, reject) => {
     const proc = spawn(command, args, options)
     proc.stdout.setEncoding('utf8')
-    proc.stdout.on('data', (data) => {
+    proc.stdout.on('data', data => {
       if (!quiet) {
         console.log(data)
       }
     })
     proc.stderr.setEncoding('utf8')
-    proc.stderr.on('data', (data) => {
+    proc.stderr.on('data', data => {
       console.error(data)
     })
-    proc.on('close', (code) => {
+    proc.on('close', code => {
       if (code !== 0) {
         reject(code)
       } else {
@@ -31,17 +33,15 @@ function spawnPromisified(command, args, { quiet = false, ...options } = {}) {
 await (async () => {
   // If dependencies are not vendored-in, install them at runtime.
   try {
-    await fs.accessSync(
-      url.fileURLToPath(new URL('./node_modules', import.meta.url)),
-      fs.constants.R_OK
-    )
+    await fs.accessSync(url.fileURLToPath(new URL('./node_modules', import.meta.url)), fs.constants.R_OK)
   } catch {
     try {
       await spawnPromisified('npm', ['ci'], {
         cwd: url.fileURLToPath(new URL('.', import.meta.url)),
-        quiet: true
+        quiet: true,
       })
-    } catch {
+    } catch (error) {
+      core.setFailed(`npm ci failed: ${error}`)
       process.exit(1)
     }
   } finally {
@@ -49,12 +49,14 @@ await (async () => {
     try {
       await spawnPromisified('npm', ['run', 'build'], {
         cwd: url.fileURLToPath(new URL('.', import.meta.url)),
-        quiet: true
+        quiet: true,
       })
-    } catch {
+    } catch (error) {
+      core.setFailed(`npm run build (TypeScript compilation) failed: ${error}`)
       process.exit(1)
     }
     // Run the main script.
+    core.info('Running file Action index.js...')
     const action = await import('./dist/index.js')
     await action.default()
   }
