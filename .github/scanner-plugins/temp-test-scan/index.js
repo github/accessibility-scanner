cb1baf657a74a823b3abde0b01704b42fd2a404c
Abdul Ahmad <abdulahmad307@github.com>
2026-04-20 11:52:56 -0400

    update test plugin so its clear why it exists

--- .github/scanner-plugins/temp-test-scan/index.js
+++
@@ -1,9 +0,0 @@
-
-// - this exist as a test to verify that loading plugins
-//   via js files still works and there are no regressions
-
-export default async function JSFilePluginLoadTest({ page, addFinding } = {}) {
-  console.log('testing loading plugin using js file')
-}
-
-export const name = 'js-file-plugin-load-test'
