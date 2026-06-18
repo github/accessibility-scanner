571b42cfae801edc5b9cf6bab75c8090a6e36f23
Taarik <147209483+taarikashenafi@users.noreply.github.com>
2026-06-18 16:37:01 -0500

    Apply suggestions from code review
    Co-authored-by: Joyce Zhu <joycezhu@github.com>

--- .github/scanner-plugins/reflow-scan/index.ts
+++ .github/scanner-plugins/reflow-scan/index.ts
@@ -15,8 +15,8 @@ export default async function reflowScan({page, addFinding} = {}) {
         problemShort: 'needs review: page presents a horizontal scrollbar at a 320px wide viewport',
         problemUrl: 'https://www.w3.org/WAI/WCAG22/Understanding/reflow.html',
         solutionShort:
-          'verify if sections of content can be viewed within the 320px wide viewport without needing to scroll in two-dimensions to read the content of an individual section',
-        solutionLong: `The page has a scroll width of ${scrollWidth}px but a client width of only ${clientWidth}px at a 320px wide viewport, resulting in a horizontal scrollbar. Ensure that multi-line text does not require scrolling in two-dimensions to read, as this would be a violation of the WCAG 1.4.10 Reflow Success Criterion.`,
+          'verify if sections of content can be viewed within the 320px wide viewport without needing to scroll in two dimensions to read the content of an individual section',
+        solutionLong: `The page has a scroll width of ${scrollWidth}px but a client width of only ${clientWidth}px at a 320px wide viewport, resulting in a horizontal scrollbar. Ensure that multi-line text does not require scrolling in two-dimensions to read, as this would be a violation of WCAG Success Criterion 1.4.10 (Reflow).`,
       })
     }
   } catch (e) {
