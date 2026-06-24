0432b26ea98a25a98f4154913e3e86097630de81
Keenan Zhou <kzhou314@github.com>
2026-06-24 13:55:11 -0700

    Fix formatting

--- .github/actions/find/src/findForUrl.ts
+++ .github/actions/find/src/findForUrl.ts
@@ -124,7 +124,11 @@ async function runAccesslintScan({
       problemShort: violation.message.toLowerCase().replace(/'/g, '&apos;'),
       problemUrl: 'https://github.com/AccessLint/accesslint/blob/main/core/README.md#rules-1',
       ruleId: violation.ruleId,
-      solutionShort: `resolve the ${violation.ruleId} violation that accesslint flagged on \`${violation.selector}\``.replace(/'/g, '&apos;'),
+      solutionShort:
+        `resolve the ${violation.ruleId} violation that accesslint flagged on \`${violation.selector}\``.replace(
+          /'/g,
+          '&apos;',
+        ),
     })
   }
 }
